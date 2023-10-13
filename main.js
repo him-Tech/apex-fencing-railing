482f024e7fb50ec77e20c81f712bfbfebdb40920
shivc8658 <shiv.kumar@tasolglobal.com>
2023-10-13 12:40:30 +0530

    swiper js added

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-     const toggleSidebarButton = document.getElementById('toggleSidebar');
-     const sidebar = document.getElementById('sidebar');
-     toggleSidebarButton.addEventListener('click', () => {
-         sidebar.classList.toggle('translate-x-full');
-     });
-     var swiper = new Swiper(".mySwiper", {
-         slidesPerView: 1,
-         centeredSlides: false,
-         slidesPerGroupSkip: 1,
-         grabCursor: true,
-         keyboard: {
-             enabled: true,
-         },
-         breakpoints: {
-             769: {
-                 slidesPerView: 2,
-                 slidesPerGroup: 2,
-             },
-         },
-         navigation: {
-             nextEl: ".swiper-button-next",
-             prevEl: ".swiper-button-prev",
-         },
-
-     });
-     const searchIcon = document.getElementById("searchIcon");
-     const searchInput = document.getElementById("searchInput");
-     searchIcon.addEventListener("click", () => {
-         searchInput.classList.toggle("hidden");
-         if (!searchInput.classList.contains("hidden")) {
-             searchInput.focus();
-         }
-     });
+import Swiper from "swiper";
+// import Swiper styles
+import "swiper/css";
+const toggleSidebarButton = document.getElementById("toggleSidebar");
+const sidebar = document.getElementById("sidebar");
+toggleSidebarButton.addEventListener("click", () => {
+  sidebar.classList.toggle("translate-x-full");
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const searchIcon = document.getElementById("searchIcon");
+const searchInput = document.getElementById("searchInput");
+searchIcon.addEventListener("click", () => {
+  searchInput.classList.toggle("hidden");
+  if (!searchInput.classList.contains("hidden")) {
+    searchInput.focus();
+  }
+});
