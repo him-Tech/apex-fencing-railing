4050d7c124be4f5f04a4f4fba0b0126e8feb0824
shivc8658 <shiv.kumar@tasolglobal.com>
2023-10-15 11:27:56 +0530

    light module added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -18,6 +19,7 @@ new Swiper(".mySwiper", {
       slidesPerGroup: 2,
     },
   },
+  modules: [Navigation, Pagination],
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
